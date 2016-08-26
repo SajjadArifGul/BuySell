a1bc2a4d5b95736664adbcf8a1f43aa992e5fb4b
Sajjad Gul <sajjadarifgul@gmail.com>
2016-08-26 22:17:00 +0500

    BuySell.WebUI
    Added BuySell.WebUI module. This will act as a startup project.

---
+++ BuySell.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BuySell.WebUI.Startup))]
+namespace BuySell.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
