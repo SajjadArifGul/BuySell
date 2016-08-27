27efb452259942818b2ed0e1d69826b91cf9b632
Sajjad Gul <sajjadarifgul@gmail.com>
2016-08-27 13:25:11 +0500

    :link: Added References
    Added References to WebUI of all libraries in solution.

---
+++ BuySell.WebUI/Controllers/VehicleAdController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BuySell.WebUI.Controllers
+{
+    public class VehicleAdController : Controller
+    {
+        // GET: VehicleAd
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Post()
+        {
+            return View();
+        }
+    }
+}
