60b43c89a81f9945d15a59b4aad6ca03ab7cdb7a
Sajjad Gul <sajjadarifgul@gmail.com>
2016-09-03 16:48:08 +0500

    :bike: :mobile_phone_off: Bike+CellPhone
    Added Bike & CellPhone Ad Views in Dashboard panel.

---
+++ BuySell.WebUI/Areas/Dashboard/Controllers/BikesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BuySell.DAL.Data;
+using BuySell.Models;
+
+namespace BuySell.WebUI.Areas.Dashboard.Controllers
+{
+    public class BikesController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Dashboard/Bikes
+        public ActionResult Index()
+        {
+            var bikes = db.Bikes.Include(b => b.Ad).Include(b => b.Color).Include(b => b.VehicleBrand).Include(b => b.Year);
+            return View(bikes.Take(50).ToList().OrderByDescending(a=>a.Ad.PostingTime));
+        }
+
+        // GET: Dashboard/Bikes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bike bike = db.Bikes.Find(id);
+            if (bike == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bike);
+        }
+
+        // POST: Dashboard/Bikes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bike bike = db.Bikes.Find(id);
+            db.Bikes.Remove(bike);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BuySell.WebUI/Areas/Dashboard/Controllers/CellPhonesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BuySell.DAL.Data;
+using BuySell.Models;
+
+namespace BuySell.WebUI.Areas.Dashboard.Controllers
+{
+    public class CellPhonesController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Dashboard/CellPhones
+        public ActionResult Index()
+        {
+            var cellPhones = db.CellPhones.Include(c => c.AccessoryBrand).Include(c => c.Ad);
+            return View(cellPhones.ToList().Take(50).OrderByDescending(a=>a.Ad.PostingTime));
+        }
+
+        // GET: Dashboard/CellPhones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CellPhone cellPhone = db.CellPhones.Find(id);
+            if (cellPhone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cellPhone);
+        }
+
+        // POST: Dashboard/CellPhones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CellPhone cellPhone = db.CellPhones.Find(id);
+            db.CellPhones.Remove(cellPhone);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
